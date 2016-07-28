11104550c55c7837c664712886edd7efe9c3d504
Ricardo Maldonado <ricomxmarketing@gmail.com>
2016-07-28 13:43:58 -0500

    Initial commit

---
+++ fr/resources/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+    <head>
+        <meta charset="UTF-8">
+        <title>FR Resources</title>
+		<link rel="stylesheet" href="http://vidaingles.com/css/bootstrap.min.css"/>
+		<link rel="stylesheet" href="http://vidaingles.com/css/jquery-ui.min.css"/>
+		<link rel="stylesheet" href="http://vidaingl;es.com/css/vi_core.css"/>
+		<link rel="stylesheet" href="http://vidaingles.com/css/vi_print.css" media="print"/>
+		<script src="http://vidaingles.com/js/jquery-1.11.3.min.js"></script>
+		<script src="http://vidaingles.com/js/bootstrap.min.js"></script>
+		<script src="http://vidaingles.com/js/jquery-ui.min.js"></script>
+		<script src="http://vidaingles.com/js/timeago.js"></script>
+		<script src="http://vidaingles.com/fr/resources/js/resources.js"></script>
+        <style type="text/css">
+            #top {
+            position: fixed;
+            bottom: 20px;
+            right: 30px;
+            ddisplay: none;
+            }
+            footer {
+            position:fixed;
+            bottom: 0px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <h1>For Real Resources</h1>
+            <ul>
+            <li><a href="elem_starters.php">Elementary Starter A & B</a></li>
+            	<li><a href="#">Pre-Intermediate Starter A & B</a></li>
+            </ul>
+        </div>
+        <footer class="footer">
+            <div class="container">
+                Vida Ingles <?php echo date('Y'); ?>
+            </div>
+        </footer>
+    </body>
+</html>
