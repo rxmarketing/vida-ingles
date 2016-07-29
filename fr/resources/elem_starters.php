10d140f8337f9795bc4ed583373151553b55555b
Ricardo Maldonado <ricomxmarketing@gmail.com>
2016-07-29 11:10:25 -0500

    pdf icon link added.

--- fr/resources/elem_starters.php
+++ fr/resources/elem_starters.php
@@ -3,6 +3,9 @@
     <head>
         <meta charset="UTF-8">
         <title>Elementary A - Starter A and B</title>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="shortcut icon" href="../../../favicon.ico">
 		<link rel="stylesheet" href="http://vidaingles.com/css/bootstrap.min.css"/>
 		<link rel="stylesheet" href="http://vidaingles.com/css/jquery-ui.min.css"/>
 		<link rel="stylesheet" href="http://vidaingles.com/css/vi_core.css"/>
