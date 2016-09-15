8d8bc67d19beecef2dcdd49f437b1f6a5be1187e
Ricardo Maldonado <ricomxmarketing@gmail.com>
2016-09-14 20:00:33 -0500

    Modified courses price per hour. Payment logic code.

--- course.php
+++ course.php
@@ -7,60 +7,69 @@
 		//$u = preg_replace('#[^a-z0-9]#i', '', $_GET['u']);
 		$courseid = preg_replace('#[^0-9]#', '', $_GET['id']);
 		} else {
-    header("location: login.php");
-    exit();	
-	}
+        header("location: login.php");
+        exit();	
+    }
 	
 	// fetch one course to display
-	$sql = "SELECT cursos.courseid, cursos.coursename AS Curso, cursos.coursedesc AS Descripcion, categorias.catname AS Category, cursos.cefdesc, cursos.competences, cursos.courseweeks, cursos.course_price AS Precio, cursos.course_hours_week AS 'Horas Semana', cursos.price_per_hour AS 'Precio Hora', cursos.temario_id, cursos.lastmodified
+	$sql = "SELECT cursos.courseid, cursos.coursename AS Curso, cursos.coursedesc AS Descripcion, categorias.catname AS Category, cursos.cefdesc, cursos.competences, cursos.total_units, cursos.hours_per_unit, cursos.incluye, cursos.hours_per_week, cursos.price_per_hour AS 'Precio Hora', cursos.temario_id, cursos.lastmodified
 	FROM cursos
 	INNER JOIN categorias
 	ON cursos.course_cat_id = categorias.catid
 	WHERE cursos.courseid ='$courseid'
 	LIMIT 1
 	";
-    // PAYMENT LOGIC ============================
-    // weeks per unit = $total_weeks / total_units
-    // hours_per_forknight = hours_per_week * 2
-    // hours_per_month = hours_per_week * 4
-    // $total_hours = hours_per_week * total_units
-    // total_weeks = total_hours / hours_per_week
-    // total_price = total_hours * price_per_hour
-    // monthly_payment = price_per_hour * hours_per_month
-    // forknight_payment = price_per_hour * hours_per_forknight
-    // weekly_payment = price_per_hour * hours_per_week
-    // -------------------------------------------------------
-    
-    // DISCOUNTS LOGIC
-    
-    // if pay monthly: 20% disc.
-        // monthly_payment - (monthly_payment * .2)
-        
-    // if pay forknight: 10% disc.
-        // forknight_payment - (forknight_payment * .1)
-        
-    // if pay weekly: 0% disc.
-        // weekly_payment - (weekly_payment * .0)
     
 	$course_query = mysqli_query($db_conx,$sql);
 	while ($row = mysqli_fetch_array($course_query, MYSQLI_ASSOC)) {
 		$course_id = $row["courseid"];
+		$temario_id = $row["temario_id"];
 		$coursename = $row["Curso"];
 		$course_desc = $row["Descripcion"];
 		$coursecat = $row["Category"];
 		$cef_desc = $row["cefdesc"];
 		$competences = $row["competences"];
-		$course_weeks = $row["courseweeks"];
-		$course_hrs_week = $row["Horas Semana"];
+        $incluye = $row["incluye"];
+		$total_units = $row["total_units"];
+		$hours_per_unit = $row["hours_per_unit"];
+		$hours_per_week = $row["hours_per_week"];
 		$course_price_hour = $row["Precio Hora"];
-		$temario_id = $row["temario_id"];
-		$total_hours = $course_hrs_week * $course_weeks;
-		$total_price = $total_hours * $course_price_hour;
-		$weekly_price = $course_hrs_week * $course_price_hour;
-		$forknight_price = 8 * $course_price_hour;
-	}
+        // PAYMENT LOGIC ============================
+        $total_hours = $hours_per_unit * $total_units;
+        $total_weeks = $total_hours / $hours_per_week;
+        $weeks_per_unit = $total_weeks / $total_units;
+        $hours_per_forknight = $hours_per_week * 2;
+        $hours_per_month = $hours_per_week * 4;
+        $total_price = $total_hours * $course_price_hour;
+        $monthly_payment = $course_price_hour * $hours_per_month;
+        $forknightly_payment = $course_price_hour * $hours_per_forknight;
+        $weekly_payment = $course_price_hour * $hours_per_week;
+        // -------------------------------------------------------
+        
+        // DISCOUNTS LOGIC
+        //switch($payment_plan){
+        //    case 1:
+        //        $payment = $weekly_payment;
+        //        break;
+        //    case 2:
+        //        //
+        //        break;
+        //    case 3:
+        //        //
+        //        break;
+        //    default
+        //        //
+        //    }
+        
+        // if pay forknight: 10% disc.
+        // forknight_payment - (forknight_payment * .1)
+        
+        // if pay weekly: 0% disc.
+        // weekly_payment - (weekly_payment * .0)
+    }
     //BUILD THE LINK TO THE TEMARIO
     $temario_link = '<a class="btn btn-primary" href="'.$domain.'/temario.php?temario_id='.$temario_id.'">Ver temario &raquo;</a>';
+    setlocale(LC_MONETARY,"es_MX");
 ?>
 <!doctype html>
 <html lang="es">
@@ -87,8 +96,8 @@
 		<!--[if lt IE 9]>
 			<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
 			<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-		<![endif]-->
-	</head>
+        <![endif]-->
+    </head>
 	<body>
 		<?php include_once('templates/template_header.php') ?>
 		<!-- SHOWCASE =======================================================-->
@@ -98,27 +107,29 @@
 			<div class="row">
 				<div class="col-md-8">
 					<h1>Cursos</h1>
-					<h3 id="exam-name"><?php echo $coursename ?></h3>
+					<h3 id="exam-name"><?php echo $coursename . '<sup><small> $'. $weekly_payment . ' semanal</small></sup>';?></h3>
 					<div class="metadata">
-						<span class="cef">CEF: <a href="pagina.php?id=1" class=""><?php echo $cef_desc ?></a> &nbsp;|&nbsp;Categor&iacute;a: <?php echo $coursecat ?> &nbsp;|&nbsp;Semanas: <?php echo $course_weeks . ' ('. $course_hrs_week .'-hrs/sem)' ?> &nbsp;|&nbsp;Horas: <?php echo $total_hours; ?>&nbsp;|&nbsp;Pago quincenal: <span class="text-danger">$<?php echo $forknight_price ?></span></span>
-					</div>
+						<span class="cef">CEF: <a href="pagina.php?id=1" class=""><?php echo $cef_desc ?></a> &nbsp;|&nbsp;Categor&iacute;a: <?php echo $coursecat ?> &nbsp;|&nbsp;Semanas: <?php echo $total_weeks . ' ('. $hours_per_week .'-hrs/sem)' ?> &nbsp;|&nbsp;Total horas: <?php echo $total_hours; ?>&nbsp;|&nbsp;<span class="text-success"><strong>Pago quincenal: <?php echo $forknightly_payment; ?></strong></span></span></span>
+                    </div>
+                    
+					<?php echo 'Precio incluye: '.$incluye ?>
 					<?php echo $course_desc ?>
 					<?php echo $competences ?>
 					<a class="btn btn-primary" href="<?php echo $domain; ?>/contacto" role="button">Solicita m&aacute;s informaci&oacute;n &raquo;</a><?php echo $temario_link; ?>
 					<?php include_once('ads/ad_responsive.php')?>
-				</div><!-- Ends col-md-8 -->
+                </div><!-- Ends col-md-8 -->
 				<!-- SIDE BAR 
-				======================================================-->
+                ======================================================-->
 				<div class="col-md-4">
 					<?php include_once('templates/template_sidebar_right.php')?>
-				</div><!--Ends col-md-4 -->
-			</div>
-		</div>
+                </div><!--Ends col-md-4 -->
+            </div>
+        </div>
 		<!--- FOOTER ======================================================-->
 		<footer class="footer">
 			<div class="container">
 				<?php include_once('templates/template_footer.php') ?>
-			</div>
-		</footer>
-	</body>
+            </div>
+        </footer>
+    </body>
 </html>				
