26d20152548b7a3101f1a37d975b9b0d30a5ffe1
Ricardo Maldonado <ricomxmarketing@gmail.com>
2016-09-23 18:33:45 -0500

    Forgot Password feature updated.

--- forgotpass.php
+++ forgotpass.php
@@ -2,34 +2,52 @@
 	include_once("inc/checar_sesion.php");
 	include_once("inc/functions.php");
 	if($user_ok == true){
-		header("location: user.php?u=".$_SESSION["username"]);
+		header("location: usuario/".$_SESSION["username"]);
 		exit();
-	}
+    }
 	// EMAIL LINK CLICK CALLS THIS CODE TO EXECUTE
 	if(isset($_GET['u']) && isset($_GET['p'])){
-		$u = mysqli_real_escape_string($db_conx, $_GET['u']);
+		$u = mysqli_real_escape_string($db_conx,$_GET['u']);
 		$temppasshash = mysqli_real_escape_string($db_conx, $_GET['p']);
-		if(strlen($temppasshash) < 10){
-			exit();
-		}
-		$sql = "SELECT userid FROM usuarios WHERE username='$u' AND temp_pass='$temppasshash' LIMIT 1";
-		$query = mysqli_query($db_conx, $sql);
-		$numrows = mysqli_num_rows($query);
-		if($numrows == 0){
-			header("location: message.php?msg=No existe un nombre de usuario con esta contraseña temporal. No puedo continuar.");
-			exit();
+		if(strlen($temppasshash) < 128){
+            echo "Password lenght issues.";
+			exit;
+        }
+		$consulta = "SELECT userid FROM usuarios WHERE username='$u' AND temp_pass='$temppasshash' LIMIT 1";
+		if(!$sql = $db_conx->prepare($consulta)){
+            echo "Los sentimos, el sitio esta experimentando problemas. Intentalo mas tarde.";
+            echo "Fallo en la preparacon (" . $db_conx->errno . ") " . $db_conx->error;
+            exit;
+        }
+        $sql->execute();
+        if(!$resultado = $sql->get_result()){
+            echo "El sitio esta teniendo problemas. Intentalo mas tarde";
+            echo "Error obteniendo los resultados (" . $db_conx->errno . ") " . $db_conx->error;
+            exit;
+        }
+        $rowCount = $resultado->num_rows;
+		if($rowCount == 0){
+			header("location: message.php?msg=no_exist");
+			exit;
 			} else {
-			while($row = mysqli_fetch_array($query, MYSQLI_ASSOC)) {
+			while($row = $resultado->fetch_assoc()){
 				$id = $row['userid'];
-			}
-			$sql1 = "UPDATE usuarios SET password='$temppasshash' WHERE userid='$id' AND username='$u' LIMIT 1";
-			$query1 = mysqli_query($db_conx, $sql1);
-			$sql2 = "UPDATE usuarios SET temp_pass='' WHERE username='$u' LIMIT 1";
-			$query2 = mysqli_query($db_conx, $sql2);
-			header("location: login.php");
-			exit();
-		}
-	}
+            }
+            $consulta = $db_conx->prepare('UPDATE usuarios SET password=? WHERE userid=? AND username=? LIMIT 1');
+            $consulta->bind_param('sds',$temppasshash,$id,$u);
+            $consulta->execute();
+            //SET temp_pass to nothing
+            $consulta1 = $db_conx->prepare("UPDATE usuarios SET temp_pass='' WHERE username='$u' LIMIT 1");
+            //$consulta1->bind_param('s','');
+            $consulta1->execute();
+			//$sql1 = "UPDATE usuarios SET password='$temppasshash' WHERE userid='$id' AND username='$u' LIMIT 1";
+			//$query1 = mysqli_query($db_conx, $sql1);
+			//$sql2 = "UPDATE usuarios SET temp_pass='' WHERE username='$u' LIMIT 1";
+			//$query2 = mysqli_query($db_conx, $sql2);
+			header("location: /iniciar");
+			//exit();
+        }
+    }
 ?>
 <!DOCTYPE html>
 <html>
@@ -46,50 +64,46 @@
 		<!--[if lt IE 9]>
 			<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
 			<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-		<![endif]-->
-		
-	</head>
+        <![endif]-->
+    </head>
 	<body>
 		<header id="header-wrap" class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-			<div id="msg"></div>
-			<div class="container">
-				<div class="navbar-header">
-					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-						<span class="sr-only">Toggle navigation</span>
-					</button> <a class="navbar-brand" href="/">Vida Inglés</a>
-				</div><!-- Ends navbar-header -->
-			</div><!--Ends container -->
-		</header>
+            <div id="msg"></div>
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="sr-only">Toggle navigation</span>
+                    </button> <a class="navbar-brand" href="/">Vida Inglés</a>
+                </div><!-- Ends navbar-header -->
+            </div><!--Ends container -->
+        </header>
 		<!-- Ends header ========
-		=======================================-->
+        =======================================-->
 		<div class="container">
 			<form id="forgotpassFrm" class="form-signin" role="form">
-				<h2 class="form-signin-heading">Contrase&ntilde;a temporal</h2>
-				
-				<label for="email" id="paso1" class="control-label">Paso 1: Escribe tu correo electr&oacute;nico</label>
-				<input type="email" id="email" name="email" class="form-control data" placeholder="Escribe tu correo electr&oacute;nico" maxlenght="88" autofocus=""/>
-				
-				<label for="email" id="paso2" class="control-label">Paso 2: Revisa tu bandeja de entrada en unos minutos.</label>
-				
-				<button id="btn-forgotpass" class="btn btn-lg btn-primary btn-block" type="submit">Genera una contrase&ntilde;a temporal</button> 
-				<div id="aviso"></div>
-				<div id="sm_textlogin" class="small text-login"> <br /><a href="<?php echo home() ?>/iniciar">Inicia sesi&oacute;n</a>
-				</div>
-			</form>
-		</div>
+                <h2 class="form-signin-heading">Contrase&ntilde;a temporal</h2>
+                <label for="email" id="paso1" class="control-label">Paso 1: Escribe tu correo electr&oacute;nico</label>
+                <input type="email" id="email" name="email" class="form-control data" placeholder="Escribe tu correo electr&oacute;nico" maxlenght="88" autofocus=""/>
+                <label for="email" id="paso2" class="control-label">Paso 2: Revisa tu bandeja de entrada en unos minutos.</label>
+                <button id="btn-forgotpass" class="btn btn-lg btn-primary btn-block" type="submit">Genera una contrase&ntilde;a temporal</button> 
+                <div id="aviso"></div>
+                <div id="sm_textlogin" class="small text-login"> <br /><a href="<?php echo home() ?>/iniciar">Inicia sesi&oacute;n</a>
+                </div>
+            </form>
+        </div>
 		<!--- FOOTER 
-		======================================================-->
-		<footer class="footer-fixed">
-			<div class="container">
-				<div class="microdata">
-					<a href="index.php">&copy; <?php echo date('Y'); ?><span itemscope itemtype="http://schema.org/Organization"> <span itemprop="name">Vida Inglés</span></a> 
-					</div><!-- Ends microdata -->
-				</div>
-			</footer>
-			<script src="js/ie10-viewport-bug-workaround.js"></script>
-			<?php include_once('includes/ga.php') ?>
-		</body>
-	</html>	
+        ======================================================-->
+        <footer class="footer-fixed">
+            <div class="container">
+                <div class="microdata">
+                    <a href="index.php">&copy; <?php echo date('Y'); ?><span itemscope itemtype="http://schema.org/Organization"> <span itemprop="name">Vida Inglés</span></a> 
+                    </div><!-- Ends microdata -->
+                </div>
+            </footer>
+            <script src="js/ie10-viewport-bug-workaround.js"></script>
+            <?php include_once('includes/ga.php') ?>
+        </body>
+    </html>	    
--- message.php
+++ message.php
@@ -1,56 +1,64 @@
 <?php
-$message = "";
-$msg = preg_replace('#[^a-z 0-9.:_()]#i', '', $_GET['msg']);
-if($msg == "activation_failure") {	
- $message = '<div class="bg-danger text-danger"><h3>Error en la activacion.</h3> <p>	Los siento, hubo un error al tratar de activar tu cuenta. Se nos ha notificado sobre este problema y nos pondremos en contacto contigo por email cuando se resuelva.</p></div>';
- } else if($msg == "activation_success"){	
- $message = '<div class="text-success bg-success"><h2>&iexcl;Enhorabuena!</h2> <p>Tu cuenta en Vida Ingl&eacute;s ha sido activada. <a href="login.php">Clic aqui para iniciar sesi&oacute;n</a></p></div>';
- } else {
-	$message = $msg;}
+    $message = "";
+    $msg = preg_replace('#[^a-z 0-9.:_()]#i', '', $_GET['msg']);
+    if($msg == "activation_failure") {	
+        $message = '<div class="bg-danger text-danger"><h3>Error en la activacion.</h3> <p>	Los siento, hubo un error al tratar de activar tu cuenta. Se nos ha notificado sobre este problema y nos pondremos en contacto contigo por email cuando se resuelva.</p></div>';
+        } elseif($msg == "activation_success"){	
+        $message = '<div class="text-success bg-success"><h2>&iexcl;Enhorabuena!</h2> <p>Tu cuenta en Vida Ingl&eacute;s ha sido activada. <a href="login.php">Clic aqui para iniciar sesi&oacute;n</a></p></div>';
+        } elseif($msg == "no_exist"){
+        $message = '<div class="text-warning bg-warning">No existe usuario con esta contrase&ntilde;a temporal. Good bye!</div>';
+        } else {
+        $message = $msg;
+    }
 ?>
 
 <!doctype html>
 <html lang="es">
-<head>	
-<meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width,initial-scale=1">
-<link rel="shortcut icon" href="favicon.ico">	
-<title>Vida Ingl&eacute;s</title>
-	<?php include("css/css_includes.php") ?> 	
- <?php include("js/javascript_includes.php") ?>  
- <!--[if lt IE 9]>  
- <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>  <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>  <![endif]--></head>
- <body>
- <header id="header-wrap" class="navbar navbar-inverse navbar-fixed-top" role="navigation">	
- <div class="container">		
- <div class="navbar-header">		
-	<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">   
- <?php echo $log_user_pic .'<span class="caret"></span>' ?>				
- <span class="sr-only">Toggle navigation</span>			</button>			<img class="logo" src="i/vidaingles-logo50x50.png" width="25" height="25" alt="Vida Ingl&eacute;s"/> <a class="navbar-brand" href="index.php">Vida Ingl&eacute;s</a>		</div>
- <!-- Ends navbar-header -->		
- <?php //include_once('vi_templates/vi_template_login_links.php') ?>	</div>
- <!--Ends container -->
- </header>
- <!-- Ends header ===============================================-->
- <div class="container"> <div class="row">  
-  <div class="col-md-6 col-md-offset-3">   
-   <div class="form" role="form">    
-    <h1 class="form-heading"></h1>    
-    <h4 class="form-subheading"></h4>   
-    <?php echo $message; ?>  
-    </div> 
-    </div>
-    </div>
- <!--- FOOTER ======================================================-->    
- <div class="container" id="register-footer">     
-  <div class="row">     
-   <div class="col-md-12">      
-    <div class="microdata">       
-     <a href="<?php echo home(); ?>">&copy; <?php echo date('Y'); ?><span itemscope itemtype="http://schema.org/Organization"> <span itemprop="name"><?php echo $site_name ?></span></a>       
-    </div><!-- Ends microdata -->      
-   </div>
-  </div>
- </div>
-</body>
-</html>
+    <head>	
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width,initial-scale=1">
+        <link rel="shortcut icon" href="favicon.ico">	
+        <title>Message</title>
+        <link rel="stylesheet" href="css/bootstrap.min.css"/>
+        <link rel="stylesheet" href="css/vi_core.css"/>
+        <!--[if lt IE 9]>  
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>  <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>  <![endif]--></head>
+        <body>
+            <header id="header-wrap" class="navbar navbar-inverse navbar-fixed-top" role="navigation">	
+                <div class="container">		
+                    <div class="navbar-header">		
+                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">   
+                            <?php //echo $log_user_pic .'<span class="caret"></span>' ?>				
+                        <span class="sr-only">Toggle navigation</span>			</button>			
+                        <img class="logo" src="i/vidaingles-logo50x50.png" width="25" height="25" alt="Vida Ingl&eacute;s"/> <a class="navbar-brand" href="index.php">Vida Ingl&eacute;s</a>		
+                    </div>
+                    <!-- Ends navbar-header -->		
+                    <?php //include_once('vi_templates/vi_template_login_links.php') ?>	
+                </div>
+                <!--Ends container -->
+            </header>
+            <!-- Ends header ===============================================-->
+            <div class="container"> 
+                <div class="row">  
+                    <div class="col-md-6 col-md-offset-3">   
+                        <div class="form" role="form">    
+                            <h1 class="form-heading"></h1>    
+                            <h4 class="form-subheading"></h4>   
+                            <?php echo $message; ?>  
+                        </div> 
+                    </div>
+                </div>
+            </div>
+            <!--- FOOTER ======================================================-->    
+            <div class="container" id="register-footer">     
+                <div class="row">     
+                    <div class="col-md-12">      
+                        <div class="microdata">       
+                            <a href="http://vidingles.com">&copy; <?php echo date('Y'); ?><span itemscope itemtype="http://schema.org/Organization"> <span itemprop="name">Vida Ingles</span></a>       
+                            </div><!-- Ends microdata -->      
+                        </div>
+                    </div>
+                </div>
+            </body>
+        </html>                                            
