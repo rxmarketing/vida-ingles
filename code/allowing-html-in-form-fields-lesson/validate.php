f0371e2c5dc0d75bef46305db88b4fc1b6df7382
Ricardo Maldonado <ricomxmarketing@gmail.com>
2016-09-22 21:23:51 -0500

    Untracked more files and directories.

--- code/allowing-html-in-form-fields-lesson/validate.php
+++
@@ -1,4 +0,0 @@
-<?php
-$text = $_POST['comment'];
-echo trim(strip_tags($text, '<b><u><i><strong>'));
-?>
--- rsss.php
+++
@@ -1,9 +0,0 @@
-<?php 
-header('Content-Type: application/rss+xml; charset=ISO-8859-1');
-	echo '<?xml version="1.0" encoding="ISO-8859-1" ?>' . "\n"	
-?>
-<rss version="2.0">
-	<channel>
-		
-	</channel>
-</rss>
