7dd36f28e1c55cb3e83aa9b6756d365c9ff2c63e
ricomx <ricomxmarketing@gmail.com>
2017-03-01 12:43:16 -0600

    modified .gitignore file

--- ceteci/inc/db_ceteci_conn.php
+++ ceteci/inc/db_ceteci_conn.php
@@ -1,8 +1,9 @@
-<?php 
+<?php
+
  $db_conx = new mysqli("localhost","ricardo","tiotony","ceteci");
     $db_conx->set_charset("utf8");
     if($db_conx->connect_errno) {
-        echo "Lo sentimos, este sitio esta teniendo problemas, intentelo mas tarde. <br />";
+        echo "Lo sentimos, este sitio esta teniendo problemas, pruebe mas tarde. <br />";
         echo "ERRNO: " . $db_conx->connect_errno . "<br />";
         echo "ERROR: " . $db_conx->connect_error;
         exit;
