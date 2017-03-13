af91f87f0dd54acb8d0c13fa4136bdc944578244
ricomx <ricomxmarketing@gmail.com>
2017-03-12 23:38:04 -0500

    removed jquery-migrate-3 link in ceteci/admin/student_statuses.php

--- ceteci/admin/student_statuses.php
+++ ceteci/admin/student_statuses.php
@@ -16,7 +16,6 @@
 		<script src="../../bower_components/jquery/dist/jquery.min.js"></script>
 		<script src="../../bower_components/tether/dist/js/tether.min.js"></script>
 		<script src="../../bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
-		<script src="../../js/jquery-migrate-3.js"></script>
 	</head>
 	<body>
 		<div class="container">
