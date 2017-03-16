135c4d5b7198137c88f3bf551aa8b5d4aeb4db99
ricomx <ricomxmarketing@gmail.com>
2017-03-15 22:48:54 -0500

    Reformat and rearranged code.

--- ceteci/admin/php/php_insert_group_statuses.php
+++ ceteci/admin/php/php_insert_group_statuses.php
@@ -1,15 +1,12 @@
-<?php 
-	include('../../inc/db_ceteci_conn.php');
-	$stmt = "INSERT INTO grupo_estatuses (
-													grupo_estatus_nombre
-													,grupo_estatus_fecha_creada
+<?php
+include('../../inc/db_ceteci_conn.php');
+$stmt = "INSERT INTO grupo_estatuses (grupo_estatus_nombre,grupo_estatus_fecha_creada
 													)
 													VALUES (
-														'".$_POST["grp_status_name"]."',
+														'" . $_POST["grp_status_name"] . "',
 														NOW()
 													)
 						";
-	if(mysqli_query($db_conx, $stmt)){
-		echo "Data inserted successfully!";
-	};
-?>
+if (mysqli_query($db_conx, $stmt)) {
+    echo "Data inserted successfully!";
+};
