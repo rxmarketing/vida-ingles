6e1f31c646f65bc6f435e2992e65f431e4e40c64
ricomx <ricomxmarketing@gmail.com>
2017-03-18 15:05:37 -0500

    Code clean up.

--- ceteci/admin/php/php_insert_group_statuses.php
+++ ceteci/admin/php/php_insert_group_statuses.php
@@ -1,12 +1,7 @@
 <?php
 include('../../inc/db_ceteci_conn.php');
-$stmt = "INSERT INTO grupo_estatuses (grupo_estatus_nombre,grupo_estatus_fecha_creada
-													)
-													VALUES (
-														'" . $_POST["grp_status_name"] . "',
-														NOW()
-													)
-						";
+$stmt = "INSERT INTO grupo_estatuses (grupo_estatus_nombre,grupo_estatus_fecha_creada)
+		VALUES ('" . $_POST["grp_status_name"] . "', NOW())";
 if (mysqli_query($db_conx, $stmt)) {
     echo "Data inserted successfully!";
 };
