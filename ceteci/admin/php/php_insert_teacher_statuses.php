66ec0473606d4c45997ee94893fce31a3bfdba3a
ricomx <ricomxmarketing@gmail.com>
2017-03-19 16:47:28 -0500

    View, add teacher statuses functionality page added.

---
+++ ceteci/admin/php/php_insert_teacher_statuses.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ricardo M
+ * Date: 19/03/2017
+ * Time: 04:32 PM
+ */
+include('../../inc/db_ceteci_conn.php');
+$stmt = "INSERT INTO maestro_estatuses (
+													maes_estatus_nombre
+													,maes_estatus_fecha_creada
+													)
+													VALUES (
+														'" . $_POST["maes_status_name"] . "',
+														NOW()
+													)
+						";
+if (mysqli_query($db_conx, $stmt)) {
+    echo "Data inserted successfully!";
+};
