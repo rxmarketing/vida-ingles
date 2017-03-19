79a2b4a11eaa14f17939d2eff9b23b5606468f3f
ricomx <ricomxmarketing@gmail.com>
2017-03-19 18:52:00 -0500

    Created and tracking.

---
+++ ceteci/admin/php/php_insert_mod_cat.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ricardo M
+ * Date: 19/03/2017
+ * Time: 05:51 PM
+ */
+include('../../inc/db_ceteci_conn.php');
+$id = $_POST["id"];
+$text = $_POST["text"];
+$column_name = $_POST["column_name"];
+$sql = "UPDATE modulo_categorias SET " . $column_name . "='" . $text . "' WHERE modulo_cat_id='" . $id . "'";
+if (mysqli_query($db_conx, $sql)) {
+    echo 'Data Updated';
+}
