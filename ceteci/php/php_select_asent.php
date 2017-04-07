c5c5d8b13fdce51d99a53ce4262c6c972ce5490e
ricomx <ricomxmarketing@gmail.com>
2017-04-07 18:20:42 -0500

    XMLHttpRequest access only.

--- ceteci/php/php_select_asent.php
+++ ceteci/php/php_select_asent.php
@@ -1,15 +1,20 @@
 <?php
 
-/* Ajax code executes select asentamientos options
+if (isset($_SERVER['HTTP_X_REQUESTED_WITH']) && $_SERVER['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest') {
+
+    /* Ajax code executes select asentamientos options
 	--------------------------------------------------------------------------------*/
-include_once '../classes/Database.php';
+    include_once '../classes/Database.php';
 
-$getAsent = new \dbconn\Database();
+    $getAsent = new \dbconn\Database();
 
-$muniid = $_POST['muniid'];
+    $muniid = $_POST['muniid'];
 
-$getRows = $getAsent->getRows("SELECT * FROM asent WHERE muni_id = ?", [$muniid]);
-echo '<option value="" selected="selected" disabled="disabled">-- Elige uno de la lista -----</option>';
-foreach ($getRows as $row) {
-    echo '<option value="' . $row['asent_id'] . '">' . $row['asentamiento_nombre'] . ' (' . $row['asentamiento_cp'] . ')</option>';
+    $getRows = $getAsent->getRows("SELECT * FROM asent WHERE muni_id = ?", [$muniid]);
+    echo '<option value="" selected="selected" disabled="disabled">-- Elige uno de la lista -----</option>';
+    foreach ($getRows as $row) {
+        echo '<option value="' . $row['asent_id'] . '">' . $row['asentamiento_nombre'] . ' (' . $row['asentamiento_cp'] . ')</option>';
+    }
+} else {
+    echo 'This page cannot be accessed directly!';
 }
--- ceteci/php/php_select_munis.php
+++ ceteci/php/php_select_munis.php
@@ -1,15 +1,19 @@
 <?php
 
-/* Ajax code executes select municipios options
+if (isset($_SERVER['HTTP_X_REQUESTED_WITH']) && $_SERVER['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest') {
+    /* Ajax code executes select municipios options
 	--------------------------------------------------------------------------------*/
-include_once '../classes/Database.php';
+    include_once '../classes/Database.php';
 
-$getMunis = new \dbconn\Database();
+    $getMunis = new \dbconn\Database();
 
-$estadoid = $_POST['eid'];
+    $estadoid = $_POST['eid'];
 
-$getRows = $getMunis->getRows("SELECT * FROM munis WHERE estado_id = ?", [$estadoid]);
-echo '<option value="" selected="selected" disabled="disabled">-- Elige uno de la lista -----</option>';
-foreach ($getRows as $row) {
-    echo '<option value="' . $row['muni_id'] . '">' . $row['muni_nombre'] . '</option>';
+    $getRows = $getMunis->getRows("SELECT * FROM munis WHERE estado_id = ?", [$estadoid]);
+    echo '<option value="" selected="selected" disabled="disabled">-- Elige uno de la lista -----</option>';
+    foreach ($getRows as $row) {
+        echo '<option value="' . $row['muni_id'] . '">' . $row['muni_nombre'] . '</option>';
+    }
+} else {
+    echo 'This page cannot be accessed directly.';
 }
