c53ac39380c20767626f38f126bb477cd0395de9
ricomx <ricomxmarketing@gmail.com>
2017-04-07 17:41:29 -0500

    Refactored getOptions to getRows.

--- ceteci/php/php_select_asent.php
+++ ceteci/php/php_select_asent.php
@@ -5,7 +5,7 @@
 include_once '../classes/Database.php';
 $getMunis = new \dbconn\Database();
 $muniid = $_POST['muniid'];
-$getRows = $getMunis->getOptions("SELECT * FROM asent WHERE muni_id = ?", [$muniid]);
+$getRows = $getMunis->getRows("SELECT * FROM asent WHERE muni_id = ?", [$muniid]);
 foreach ($getRows as $row) {
     echo '<option value="' . $row['asent_id'] . '">' . $row['asentamiento_nombre'] . ' (' . $row['asentamiento_cp'] . ')</option>';
 }
