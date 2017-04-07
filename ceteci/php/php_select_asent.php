8a02f6c5259009ce647672217e26d323b44b743e
ricomx <ricomxmarketing@gmail.com>
2017-04-07 17:44:41 -0500

    Rename variable.

--- ceteci/php/php_select_asent.php
+++ ceteci/php/php_select_asent.php
@@ -3,9 +3,9 @@
 /* Ajax code executes select asentamientos options
 	--------------------------------------------------------------------------------*/
 include_once '../classes/Database.php';
-$getMunis = new \dbconn\Database();
+$getAsent = new \dbconn\Database();
 $muniid = $_POST['muniid'];
-$getRows = $getMunis->getRows("SELECT * FROM asent WHERE muni_id = ?", [$muniid]);
+$getRows = $getAsent->getRows("SELECT * FROM asent WHERE muni_id = ?", [$muniid]);
 foreach ($getRows as $row) {
     echo '<option value="' . $row['asent_id'] . '">' . $row['asentamiento_nombre'] . ' (' . $row['asentamiento_cp'] . ')</option>';
 }
