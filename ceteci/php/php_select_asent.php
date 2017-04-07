335339fdb2ae806008bde8736e4db4e16ecbb754
ricomx <ricomxmarketing@gmail.com>
2017-04-07 18:02:22 -0500

    Updated code to PHP7 PDO.

--- ceteci/php/php_select_asent.php
+++ ceteci/php/php_select_asent.php
@@ -3,9 +3,13 @@
 /* Ajax code executes select asentamientos options
 	--------------------------------------------------------------------------------*/
 include_once '../classes/Database.php';
+
 $getAsent = new \dbconn\Database();
+
 $muniid = $_POST['muniid'];
+
 $getRows = $getAsent->getRows("SELECT * FROM asent WHERE muni_id = ?", [$muniid]);
+echo '<option value="" selected="selected" disabled="disabled">-- Elige uno de la lista -----</option>';
 foreach ($getRows as $row) {
     echo '<option value="' . $row['asent_id'] . '">' . $row['asentamiento_nombre'] . ' (' . $row['asentamiento_cp'] . ')</option>';
 }
--- ceteci/php/php_select_munis.php
+++ ceteci/php/php_select_munis.php
@@ -1,20 +1,15 @@
 <?php
-    $db_conx = new mysqli("localhost","ricomx","tiotony","cetec");
-    $db_conx->set_charset("utf8");
-    if($db_conx->connect_errno) {
-        echo "Lo sentimos, este sitio esta teniendo problemas, intentelo mas tarde. <br />";
-        //echo "ERRNO: " . $db_conx->connect_errno . "<br />";
-        //echo "ERROR: " . $db_conx->connect_error;
-        exit;
-    }
-    
-    //Ajax code executes select muni options
-    $estadoid = $_POST['eid'];
-    $muniqry = mysqli_query($db_conx, "SELECT * FROM munis WHERE estado_id = '$estadoid'");
-    if(mysqli_num_rows($muniqry)>0) {
-        echo '<option value="" selected="selected" disabled="disabled">-- Choose one from the list -----</option>';
-        while($row = mysqli_fetch_array($muniqry, MYSQLI_ASSOC)) {
-            echo '<option value="'.$row['muni_id'].'">'.$row['muni_nombre'].'</option>';
-        }
-    }
-?>
+
+/* Ajax code executes select municipios options
+	--------------------------------------------------------------------------------*/
+include_once '../classes/Database.php';
+
+$getMunis = new \dbconn\Database();
+
+$estadoid = $_POST['eid'];
+
+$getRows = $getMunis->getRows("SELECT * FROM munis WHERE estado_id = ?", [$estadoid]);
+echo '<option value="" selected="selected" disabled="disabled">-- Elige uno de la lista -----</option>';
+foreach ($getRows as $row) {
+    echo '<option value="' . $row['muni_id'] . '">' . $row['muni_nombre'] . '</option>';
+}
