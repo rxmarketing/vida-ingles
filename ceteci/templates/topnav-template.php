f13797429f4a67af518a0dec1df55ac522d81dce
ricomx <ricomxmarketing@gmail.com>
2017-04-13 13:02:07 -0500

    Initial commit.

---
+++ ceteci/templates/topnav-template.php
@@ -0,0 +1,46 @@
+<nav class="navbar navbar-toggleable-md navbar-inverse bg-inverse fixed-top">
+	<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+		<span class="navbar-toggler-icon"></span>
+	</button>
+	<a class="navbar-brand" href="#">CETEC Dashboard</a>
+	<div class="collapse navbar-collapse" id="navbarsExampleDefault">
+		<ul class="navbar-nav mr-auto">
+			<li class="nav-item active">
+				<a class="nav-link" href=""><span class="fa fa-home fa-lg"></span> <span class="sr-only">(current)</span></a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="groups.php"><span class="fa fa-book fa-lg"></span> Grupos</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="students.php">Estudiantes</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="modules.php">Módulos</a>
+			</li>
+			<li class="nav-item dropdown">
+				<a class="nav-link dropdown-toggle" href="http:" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+				<div class="dropdown-menu" aria-labelledby="dropdown01">
+					<a class="dropdown-item" href="#">Action</a>
+					<a class="dropdown-item" href="#">Another action</a>
+					<a class="dropdown-item" href="#">Something else here</a>
+				</div>
+			</li>
+		</ul>
+		<ul class="navbar-nav my-2 my-md-0">
+			<li class="nav-item"><a class="nav-link" href="#">Crear cuenta</a></li>
+			<li class="nav-item"><a class="nav-link" href="#">Iniciar sesión</a></li>
+			<li class="nav-item">
+				<div class="dropdown">
+					<a class="dropdown-toggle nav-link" type="" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+						<span class="fa fa-ellipsis-v fa-lg"></span>
+					</a>
+					<div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuButton">
+						<a class="dropdown-item" href="#">Action</a>
+						<a class="dropdown-item" href="#">Another action</a>
+						<a class="dropdown-item" href="#">Something else here</a>
+					</div>
+				</div>
+			</li>
+		</ul>
+	</div>
+</nav>
