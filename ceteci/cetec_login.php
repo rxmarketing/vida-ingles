6bb44a73bc6fa3d393e4a6d30e37d37445d49297
ricomx <ricomxmarketing@gmail.com>
2017-04-16 16:57:37 -0500

    Styled login form.

--- ceteci/cetec_login.php
+++ ceteci/cetec_login.php
@@ -12,23 +12,84 @@ if ($admin_ok == true) {
     exit();
 }
 ?>
-<!doctype html>
-<html lang="en">
+<!DOCTYPE html>
+<html lang="es">
 <head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<title>Login Cetec</title>
+	<meta charset="utf-8">
+	<title>Login</title>
+	<link rel="stylesheet" href="../bower_components/bootstrap/dist/css/bootstrap.min.css"/>
+	<link href="../bower_components/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+	<link rel="stylesheet" href="css/cetec.css">
+	<script src="../bower_components/jquery/dist/jquery.min.js"></script>
+	<script src="../bower_components/tether/dist/js/tether.min.js"></script>
+	<script src="../bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+	<script src="js/main.js"></script>
+	<script src="js/ceteci.js"></script>
+	<script>
+	</script>
+	<style>
+		/* Sticky footer styles
+-------------------------------------------------- */
+		html {
+			position: relative;
+			min-height: 100%;
+		}
+
+		body {
+			/* Margin bottom by footer height */
+			margin-bottom: 60px;
+		}
+
+		.form-signin {
+			max-width: 330px;
+			padding: 15px;
+			margin: 0 auto;
+		}
+
+		.form-signin input[type="email"] {
+			margin-bottom: -1px;
+			border-bottom-right-radius: 0;
+			border-bottom-left-radius: 0;
+		}
+
+		.form-signin input[type="password"] {
+			margin-bottom: 10px;
+			border-top-left-radius: 0;
+			border-top-right-radius: 0;
+		}
+
+		.footer {
+			position: absolute;
+			bottom: 0;
+			width: 100%;
+			/* Set the fixed height of the footer here */
+			height: 60px;
+			line-height: 60px; /* Vertically center the text there */
+			background-color: #f5f5f5;
+		}
+
+		/* Custom page CSS
+    -------------------------------------------------- */
+		/* Not required for template or sticky footer method. */
+	</style>
 </head>
 <body>
-<form method="post" action="php/php_cetec_login.php" id="loginform" class="form-signin" role="form">
-	<h2 class="form">Iniciar sesi&oacute;n</h2>
-	<div class="label sr-only">Correo electr&oacute;nico:</div>
-	<input type="text" name="email" id="email" class="form-control" placeholder="Correo electrónico" maxlength="88" autofocus/>
-	<div class="label sr-only">Contraseña</div>
-	<input type="password" name="password" id="password" class="form-control" placeholder="Contraseña" maxlength="100">
-	<button id="btn-signin" class="btn btn-lg btn-primary btn-block">Iniciar sesión</button>
-	<div id="status"></div>
-</form>
+<?php include_once 'templates/topnav-template.php'; ?>
+<div class="container">
+	<form method="post" action="php/php_cetec_login.php" id="loginform" class="form-signin" role="form">
+		<h2 class="form">Iniciar sesi&oacute;n</h2>
+		<div class="label sr-only">Correo electr&oacute;nico:</div>
+		<input type="text" name="email" id="email" class="form-control" placeholder="Correo electrónico" maxlength="88" autofocus/>
+		<div class="label sr-only">Contraseña</div>
+		<input type="password" name="password" id="password" class="form-control" placeholder="Contraseña" maxlength="100">
+		<button id="btn-signin" class="btn btn-lg btn-primary btn-block">Iniciar sesión</button>
+		<div id="status"></div>
+	</form>
+</div>
+<footer class="footer">
+	<div class="container">
+		<small>Dashboard v.2.0.0</small>
+	</div>
+</footer>
 </body>
 </html>
